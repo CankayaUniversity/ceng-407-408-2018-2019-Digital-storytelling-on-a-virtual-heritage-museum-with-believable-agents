b91718b705799ec4fdf74d35b3e14e51eb1973e7
Saygın Dikbayır <hdikbayir@gmail.com>
2019-04-28 21:19:55 +0300

    Add files via upload

---
+++ CanvasFreezeTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class CanvasFreezeTrigger : MonoBehaviour
+{
+    public GameObject QuestionCanvas;
+    public GameObject Karacongolos;
+    public FirstPersonController FPSContoller;
+    // public GameObject ScriptOnOff;
+    //public GameObject FPSContoller;
+    //public Animation Anim;
+  
+     
+
+
+    private void Start()
+    {
+        Karacongolos.SetActive(false);
+        FPSContoller.GetComponent<FirstPersonController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            Debug.Log("girdi");
+            Karacongolos.SetActive(true);
+            QuestionCanvas.SetActive(true);
+            FPSContoller.m_UseHeadBob = false;
+            FPSContoller.enabled = false;
+            //FPSContoller = GameObject.FindWithTag("Player");
+            //FPSContoller.GetComponent(FirstPersonController).enable = false;
+           
+
+
+
+
+        }
+    }
+}
